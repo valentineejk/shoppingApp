e2eff7eed995fd49b3dc9d2684092130f0842032
valentineejk <valentineejk@gmail.com>
2022-05-30 11:52:52 +0100

    Initial Commit

---
+++ shoppingApp/shoppingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  shoppingAppApp.swift
+//  shoppingApp
+//
+//  Created by Valentineejk on 30/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct shoppingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
